12879ab9a0284963948624317a7d53262cb14afc
Denis <denis@rob1nzon.ru>
2014-09-29 10:14:42 +0400

    update parser

--- dlastwth.py
+++ dlastwth.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import csv
+import unicodecsv
 import requests
 import urllib2
 import logging
@@ -13,46 +14,36 @@ import shutil
 
 from StringIO import StringIO
 
+def get_last_date_bd():
+    sql='''SELECT `data` FROM `weather`
+        ORDER BY `weather`.`data`  DESC
+        LIMIT 1'''
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return results[0][0]
+
+
 def unk(str):
     #return str[str.find("'"):str.rfind("']")]
     return str[2:str.rfind("']")]
 
+def expnda(base,wm):
+    print base.splitlines(1)[7:][0].split(';')[0][1:-7]
+    srx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for tline in base.splitlines(1)[7:]:
+        tarr=tline.split(';')
 
-def expnd(base, wm):
-    #i = [0, 1, 2, 3, 4, 5, 7, 10, 22, 23, 26]
-    # for l in range(len(base)):
-    #     for a in i:
-    #         print unk(base[l][a])
-    srx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for x in base:
-        #print x
-        g=range(len(srx)+1)
-        del g[0]  # костыли дата
-        #del g[5]  # облачка
-        #del g[-2] # еще
-        for j in g:
-            if (unk(x[j])!=''):
-                if (unk(x[j])!=' '):
-                    if (j==5) or (j==6) or (j==10):
-                        try:
-                            srx[j]=srx[j]+float(re.findall('(\d+)', x[j])[0]) #костыль
-                        except:
-                            print x
-                            print j,x[j]
-                            print 'Problem with format :('
-                    else:
-                        srx[j]=srx[j]+float(unk(x[j])) # складываем
-    for j in g[:-1]:
-        try:
-            if (srx[j]!=' '):
-                srx[j] = round(srx[j]/len(base)) # среднее значение
-        except:
-            print j
-            print 'Problem with round :('
+        srx[0] += float(tarr[1][1:-1])
+        srx[1] += float(tarr[2][1:-1])
+        srx[2] += float(tarr[3][1:-1])
+        srx[3] += float(tarr[4][1:-1])
+        srx[4] += float(tarr[5][1:-1])
+        srx[5] += float(tarr[7][1:-1])
+        srx[6] += float(re.findall('(\d+)', tarr[10][1:3])[0])
+        srx[7] += float(tarr[22][1:-1])
+        srx[8] += float(tarr[23][1:-1])
+        srx[9] += float(tarr[26][1:-1])
 
-    srx[0] = unk(base[0][0])[:-6]
-
-    add_to_db(wm, srx[0], srx[1], srx[2], srx[3], srx[4], srx[5], srx[6], srx[7], srx[8], srx[9], srx[10])
 
 
 def add_to_db(wm, date, temp, pa, pa2, pd, vl, ff, n, td, rrr, tg):
@@ -79,13 +70,14 @@ def load_data(wmid):
     d1 = str(old_date.day)
     y1 = str(old_date.year)
 
-    dt1 = d1 + '.' + m1 + '.' + y1
+    dt1 = get_last_date_bd()
+    dt2 = d1 + '.' + m1 + '.' + y1
     #dt2 = d2 + '.' + m2 + '.' + y2
     #print dt1,dt2
 
     data = {
         'a_date1': dt1,
-        'a_date2': dt1,
+        'a_date2': dt2,
         'f_ed3': m2,
         'f_ed4': m2,
         'f_ed5': d2,
@@ -112,42 +104,19 @@ def load_data(wmid):
 
     else:
         s = r.text
-        #print s
         a = s.find('http://')
         b = s.rfind('csv.gz') + 6
         surl=s[a:b]
         zname=surl[surl.rfind('/')+1:b]
-        #print zname
-        #urllib.urlretrieve(surl,zname)
-        #download(surl)self.ui.plainTextEdit.appendPlainText(s[a:b])
-        #print surl
         request = urllib2.Request(surl)
         request.add_header('Accept-encoding', 'gzip')
         response = urllib2.urlopen(request)
-        buf = StringIO( response.read())
+        buf = StringIO(response.read())
         f = gzip.GzipFile(fileobj=buf)
         data = f.read()
-        #self.ui.plainTextEdit.appendPlainText(_fromUtf8(data))
-        cdata = csv.reader(data)
-        n = 0
+        tdata = data.decode('utf8')
+        expnda(tdata, wmid)
 
-        a = []
-        b = []
-        y=0
-        for i, p in enumerate(cdata):
-            s = str(p)
-            #print str(p)
-            if (s.find(':')>0 and len(s)>5):
-                if (y>0):
-                    b.append(a)
-                #print a
-                del(a)
-                a = []
-                y=1
-            if (y==1):
-                if (s.rfind(';')<0):
-                    a.append(s)
-        expnd(b,wmid)
 
 def download(url):
     try:
@@ -165,9 +134,6 @@ def download(url):
                 break
             file_size_dl += len(buffer)
             f.write(buffer)
-            #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = '#'
-            #print status,
         f.close()
     except:
             logging.warning("Url error")
@@ -183,6 +149,7 @@ db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="", db="weather", cha
 cursor = db.cursor()
 filename = 'C:\Users\USER\unic1.csv'
 count=1
+get_last_date_bd()
 try:
     with open(filename, 'rb') as f:
         reader = csv.reader(f)
