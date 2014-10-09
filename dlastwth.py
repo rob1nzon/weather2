73f76584e944a95d867d350822df02b0e3f9304f
Denis <denis@rob1nzon.ru>
2014-10-09 13:23:21 +0400

    fix data change
    a

--- dlastwth.py
+++ dlastwth.py
@@ -11,7 +11,6 @@ import psycopg2
 
 from StringIO import StringIO
 
-
 def get_last_date_bd():
     sql = '''SELECT data FROM agz_.weather
         ORDER BY weather.data  DESC
@@ -20,11 +19,9 @@ def get_last_date_bd():
     results = cursor.fetchall()
     return results[0][0]
 
-
 def unk(str):
     # return str[str.find("'"):str.rfind("']")]
     return str[2:str.rfind("']")]
-
 
 def expnda(base, wm):
     def plus(x, s):
@@ -35,7 +32,7 @@ def expnda(base, wm):
                     srx[x] += float(s)
                 else:
                     if (float(s) != 0):
-                        srx[x] = (srx[x] + float(s)) / 2
+                        srx[x] = round(((srx[x] + float(s)) / 2), 2)
             else:
                 srx[x] = float(s)
         else:
@@ -43,32 +40,61 @@ def expnda(base, wm):
 
     global srx
     srx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    try:
-        for tline in base.splitlines(1)[7:]:
-            tarr = tline.split(';')
-            #print tarr[1], tarr[2], tarr[3], tarr[4], tarr[5], tarr[7], tarr[22], tarr[23], tarr[26]
-            plus(0, tarr[1][1:-1])
-            plus(1, tarr[2][1:-1])
-            plus(2, tarr[3][1:-1])
-            plus(3, tarr[4][1:-1])
-            plus(4, tarr[5][1:-1])
-            plus(5, tarr[7][1:-1])
-            plus(7, tarr[22][1:-1])
-            try:
-                plus(8, re.findall('(\d+.\d+)', tarr[23][1:-1])[0])
-            except:
-                plus(8, '')
-            try:
-                plus(9, tarr[26][1:-1])
-            except:
-                plus(9, '')
-            #print 'srx', srx
-    except:
-        print ':('
-    print 'FINAL', srx
-    add_to_db(wm, base.splitlines(1)[7:][0].split(';')[0][1:-7],
+
+    dtch=base.splitlines(1)[7:][0].split(';')[0][1:-7]
+    #print dtch
+
+    for tline in base.splitlines(1)[7:]:
+        tarr = tline.split(';')
+        if (dtch != tarr[0][1:-7]):
+            #ToDo Оптимизировать запросы к базе данных
+            add_to_db(wm, dtch,
               srx[0], srx[1], srx[2], srx[3], srx[4],
               srx[5], srx[6], srx[7], srx[8], srx[9])
+            srx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            dtch = tarr[0][1:-7]
+        #print tarr[1], tarr[2], tarr[3], tarr[4], tarr[5], tarr[7], tarr[22], tarr[23], tarr[26]
+        #ToDO Написать комментарии по каждому пункту
+        try:
+            plus(0, tarr[1][1:-1])
+        except:
+            print '1', tarr[1]
+        try:
+            plus(1, tarr[2][1:-1])
+        except:
+            print '2', tarr[2]
+        try:
+            plus(2, tarr[3][1:-1])
+        except:
+            print '3', tarr[3]
+        try:
+            plus(3, tarr[4][1:-1])
+        except:
+            print '4', tarr[4]
+        try:
+            plus(4, tarr[5][1:-1])
+        except:
+            print '5', tarr[5]
+        try:
+            plus(5, tarr[7][1:-1])
+        except:
+            print '7', tarr[7]
+        try:
+            plus(7, tarr[22][1:-1])
+        except:
+            #print '22', tarr[22]
+            plus(7, '')
+        try:
+            plus(8, re.findall('(\d+.\d+)', tarr[23][1:-1])[0])
+        except:
+            plus(8, '')
+        try:
+            plus(9, tarr[26][1:-1])
+        except:
+            plus(9, '')
+
+        #print 'srx', srx
+    #print 'FINAL', srx
 
 
 def add_to_db(wm, date, temp, pa, pa2, pd, vl, ff, n, td, rrr, tg):
@@ -76,11 +102,11 @@ def add_to_db(wm, date, temp, pa, pa2, pd, vl, ff, n, td, rrr, tg):
         VALUES ('%(w)s', '%(d)s', %(t)s,%(p)s,%(p2)s,%(pd)s,%(vl)s,%(Ff)s,%(N)s,%(Td)s,%(RRR)s,%(Tg)s)
         """ % {"w": wm, "d": date, "t": temp, "p": pa, "p2": pa2, "pd": pd, "vl": vl, "Ff": ff, "N": n, "Td": td,
                "RRR": rrr, "Tg": tg}
-    print sql
+    #print sql
     cursor.execute(sql)
 
 
-def load_data(wmid):
+def load_data(wmid, gdate):
     # metar=5001&a_date1=15.06.2014&a_date2=16.06.2014&f_ed3=6&f_ed4=6&f_ed5=15&f_pe=1&f_pe1=3&lng_id=2
     # http://rp5.ru/inc/f_metar.php?
 
@@ -96,17 +122,15 @@ def load_data(wmid):
     d1 = str(old_date.day)
     y1 = str(old_date.year)
 
-    dt1 = get_last_date_bd()
+    dt1 = gdate[8:10]+'.'+gdate[5:7]+'.'+gdate[0:4]
     dt2 = d1 + '.' + m1 + '.' + y1
-    #dt2 = d2 + '.' + m2 + '.' + y2
-    #print dt1,dt2
 
     data = {
         'a_date1': dt1,
         'a_date2': dt2,
         'f_ed3': m2,
         'f_ed4': m2,
-        'f_ed5': d2,
+        'f_ed5': '18',
         'f_pe': '1',
         'f_pe1': '2',
         'lng_id': '2',
@@ -143,36 +167,9 @@ def load_data(wmid):
         tdata = data.decode('utf8')
         expnda(tdata, wmid)
 
-
-def download(url):
-    try:
-        file_name = url.split('/')[-1]
-        u = urllib2.urlopen(url)
-        f = open('data/' + file_name, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print ("Downloading: %s Bytes: %s" % (file_name, file_size))
-        file_size_dl = 0
-        block_sz = 18192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-            file_size_dl += len(buffer)
-            f.write(buffer)
-        f.close()
-    except:
-        logging.warning("Url error")
-
-
 old = ''
-# load_data('30692')
-## log
-logging.basicConfig(filename='testcsv.log', level=logging.DEBUG)
-##
 
-#db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="", db="weather", charset='utf8')
-
+logging.basicConfig(filename='testcsv.log', level=logging.ERROR)
 
 try:
      db = psycopg2.connect("""dbname='postgis_21_sample'
@@ -195,8 +192,15 @@ d1 = str(old_date.day)
 if (len(d1)==1): d1='0'+d1
 y1 = str(old_date.year)
 old_d =  y1 + '-' + m1 + '-' + d1
-print get_last_date_bd()
-if (get_last_date_bd() != old_d):
+
+try:
+    date_bd = get_last_date_bd()
+except:
+    print 'Записей в базе не обнаружено'
+    date_bd = raw_input('С какой даты скачать архив в формате: YYYY-MM-DD:')
+    print date_bd
+
+if (str(date_bd) != old_d):
     try:
         with open(filename, 'rb') as f:
             reader = csv.reader(f)
@@ -204,13 +208,13 @@ if (get_last_date_bd() != old_d):
                 if (old != row[1]):
                     #print row[0], row[1]
                     count = count + 1
-                    load_data(row[1])
+                    load_data(row[1], date_bd)
                     old = row[1]
     except:
-        print 'Error'
+        print 'Ошибка обработки файла'
     print str(count)
 else:
-    print 'Database is relevant...'
+    print 'База данных актуальна...'
 db.commit()
 db.close()
 
